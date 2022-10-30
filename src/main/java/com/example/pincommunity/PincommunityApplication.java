f72b3cbc9019043f8e03a084bd5cd7094b619341
YakovMalykh <to-yakoff@mail.ru>
2022-10-30 21:15:51 +0300

    Initial commit

---
+++ src/main/java/com/example/pincommunity/PincommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.pincommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PincommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PincommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pincommunity/PincommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pincommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PincommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
