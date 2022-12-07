56b5d24ee2fd0d2238791322c190469ad3eb7e6c
YakovMalykh <to-yakoff@mail.ru>
2022-12-07 14:02:23 +0300

    рописал работу с фалами и сервисы по работе с аватарами и картинками, но их не до конца

---
+++ src/main/java/com/example/pincommunity/servicies/inpl/PictureServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.pincommunity.servicies.inpl;
+
+import com.example.pincommunity.models.Picture;
+import com.example.pincommunity.repositories.PictureRepository;
+import com.example.pincommunity.servicies.FileHandler;
+import com.example.pincommunity.servicies.ImageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+public class PictureServiceImpl implements ImageService<Picture> {
+    @Value("${path.to.pictures.folder}")
+    private String folderName;
+
+    private final PictureRepository pictureRepository;
+
+    public PictureServiceImpl(PictureRepository pictureRepository) {
+        this.pictureRepository = pictureRepository;
+    }
+
+    @Override
+    public Picture saveImage(MultipartFile file, String newFileName, Picture image) {
+        String filePathInFolder = FileHandler.saveFileIntoFolder(file, newFileName, folderName);
+        byte[] preview = FileHandler.generatePreview(filePathInFolder);
+        String contentType = file.getContentType();
+
+        Picture picture = new Picture();
+        picture.setFilePathInFolder(filePathInFolder);
+        picture.setMediaType(contentType);
+        picture.setPreview(preview);
+
+        Picture savedPicture = pictureRepository.save(picture);//here I need get ID, for creating url and then write it into DB
+        savedPicture.setPictureUrl(String.format("/pictures/%s" + savedPicture.getId()));
+        log.info("Picture was saved");
+        return pictureRepository.save(savedPicture);
+    }
+
+    @Override
+    public ResponseEntity<Picture> getImageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Picture> updateImage(Picture image) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteImageById(Long id) {
+        return null;
+    }
+}
