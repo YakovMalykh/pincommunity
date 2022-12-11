d6691f66d4ff6994d9eb9e02599169aa48295d21
Sascvotch <sascvotch@yandex.ru>
2022-12-11 20:01:52 +0300

    Club mapper

--- src/test/java/com/example/pincommunity/mappers/ClubMapperTest.java
+++ src/test/java/com/example/pincommunity/mappers/ClubMapperTest.java
@@ -3,7 +3,6 @@ package com.example.pincommunity.mappers;
 import com.example.pincommunity.dto.ClubDto;
 import com.example.pincommunity.models.Club;
 import com.example.pincommunity.models.Member;
-import com.example.pincommunity.repositories.ClubRepository;
 import com.example.pincommunity.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,9 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static com.example.pincommunity.Constatnts.ConstantsForTests.*;
-import static com.example.pincommunity.constants.Role.USER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -69,5 +66,20 @@ public class ClubMapperTest {
         Club club = clubMapper.createClubDtoToClub(CREATE_CLUB_DTO);
         assertEquals(CREATE_CLUB_DTO.getAdminUsername(), club.getAdmin().getUsername());
         assertEquals(CREATE_CLUB_DTO.getCity(), club.getCity());
+    }
+    @Test
+    void updateClubFromClubDto(){
+        when(memberRepository.getMemberByUsernameIgnoreCase(anyString())).thenReturn(Optional.of(MEMBER));
+
+        Club club=new Club();
+        club.setCity("Town");
+        CLUB_DTO.setCity(CLUB_CITY);
+        CLUB_DTO.setAdminUsername(EMAIL);
+        CLUB_DTO.setId(1L);
+
+        clubMapper.updateClubFromClubDto(CLUB_DTO,club);
+        assertEquals(CLUB_DTO.getCity(), club.getCity() );
+        assertEquals(CLUB_DTO.getAdminUsername(), club.getAdmin().getUsername() );
+
     }
 }
