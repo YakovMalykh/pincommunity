f4346c9bd524ad42877b60c0b12afa1538a40e61
YakovMalykh <to-yakoff@mail.ru>
2022-12-12 15:33:32 +0300

    с тестами на написанные сервисы закончил

--- src/test/java/com/example/pincommunity/servicies/AuthServiceTest.java
+++ src/test/java/com/example/pincommunity/servicies/AuthServiceTest.java
@@ -1,15 +1,12 @@
 package com.example.pincommunity.servicies;
 
-import com.example.pincommunity.Constatnts.ConstantsForTests;
 import com.example.pincommunity.dto.CreateMemberDto;
 import com.example.pincommunity.servicies.inpl.AuthServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
